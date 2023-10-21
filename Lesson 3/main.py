e6eff6d5bd5f713a2b0c7a1e2083957560b0a612
disahard <pg9677952@gmail.com>
2023-10-21 10:54:15 +0300

    L3

---
+++ Lesson 3/main.py
@@ -0,0 +1,27 @@
+from auto import Human
+from auto import Auto
+from auto import AutoType
+from auto import HumanRole
+
+humans = list()
+studone = Human('studone', HumanRole.DRIVER)
+studtwo = Human('studtwo', HumanRole.PASSENGER)
+studthree = Human('studthree', HumanRole.PASSENGER)
+studfour = Human('studfour', HumanRole.PASSENGER)
+humans.append(studone)
+humans.append(studtwo)
+humans.append(studthree)
+humans.append(studfour)
+
+audi = Auto("etron", AutoType.CAR)
+for human in humans:
+    audi.AddPassengers(human)
+    audi.AddDrivers(human)
+
+'''
+for driver in audi.Drivers:
+    print(driver)
+for passenger in audi.Passengers:
+    print(passenger)
+'''
+print(audi)
